78034a6117b836f831e35ba321a51ce5a06c53d6
vxcheese <sackcreator54@live.com>
2015-05-17 16:26:44 -0700

    Initial Commit

---
+++ VXPMenu/Form1.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace VXPMenu
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gameView = new System.Windows.Forms.Panel();
+            this.emulatorView = new System.Windows.Forms.Panel();
+            this.settingView = new System.Windows.Forms.Panel();
+            this.consoleView = new System.Windows.Forms.Panel();
+            this.powerView = new System.Windows.Forms.Panel();
+            this.power = new System.Windows.Forms.Button();
+            this.console = new System.Windows.Forms.Button();
+            this.settings = new System.Windows.Forms.Button();
+            this.emulators = new System.Windows.Forms.Button();
+            this.games = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // gameView
+            // 
+            this.gameView.AutoScroll = true;
+            this.gameView.BackColor = System.Drawing.Color.LightGray;
+            this.gameView.Location = new System.Drawing.Point(268, 202);
+            this.gameView.Name = "gameView";
+            this.gameView.Size = new System.Drawing.Size(200, 100);
+            this.gameView.TabIndex = 12;
+            // 
+            // emulatorView
+            // 
+            this.emulatorView.BackColor = System.Drawing.Color.LightGray;
+            this.emulatorView.Location = new System.Drawing.Point(268, 59);
+            this.emulatorView.Name = "emulatorView";
+            this.emulatorView.Size = new System.Drawing.Size(200, 100);
+            this.emulatorView.TabIndex = 13;
+            // 
+            // settingView
+            // 
+            this.settingView.BackColor = System.Drawing.Color.LightGray;
+            this.settingView.Location = new System.Drawing.Point(195, 120);
+            this.settingView.Name = "settingView";
+            this.settingView.Size = new System.Drawing.Size(200, 100);
+            this.settingView.TabIndex = 13;
+            // 
+            // consoleView
+            // 
+            this.consoleView.BackColor = System.Drawing.Color.LightGray;
+            this.consoleView.Location = new System.Drawing.Point(163, 208);
+            this.consoleView.Name = "consoleView";
+            this.consoleView.Size = new System.Drawing.Size(200, 100);
+            this.consoleView.TabIndex = 13;
+            // 
+            // powerView
+            // 
+            this.powerView.BackColor = System.Drawing.Color.LightGray;
+            this.powerView.Location = new System.Drawing.Point(131, 123);
+            this.powerView.Name = "powerView";
+            this.powerView.Size = new System.Drawing.Size(200, 100);
+            this.powerView.TabIndex = 13;
+            // 
+            // power
+            // 
+            this.power.BackColor = System.Drawing.Color.White;
+            this.power.BackgroundImage = global::VXPMenu.Properties.Resources.power;
+            this.power.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.power.FlatAppearance.BorderColor = System.Drawing.Color.LightGray;
+            this.power.FlatAppearance.BorderSize = 0;
+            this.power.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.power.Location = new System.Drawing.Point(0, 566);
+            this.power.Name = "power";
+            this.power.Size = new System.Drawing.Size(75, 101);
+            this.power.TabIndex = 9;
+            this.power.UseVisualStyleBackColor = false;
+            this.power.Click += new System.EventHandler(this.power_Click);
+            // 
+            // console
+            // 
+            this.console.BackColor = System.Drawing.Color.White;
+            this.console.BackgroundImage = global::VXPMenu.Properties.Resources.console;
+            this.console.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.console.FlatAppearance.BorderColor = System.Drawing.Color.LightGray;
+            this.console.FlatAppearance.BorderSize = 0;
+            this.console.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.console.Location = new System.Drawing.Point(0, 460);
+            this.console.Name = "console";
+            this.console.Size = new System.Drawing.Size(75, 100);
+            this.console.TabIndex = 8;
+            this.console.UseVisualStyleBackColor = false;
+            this.console.Click += new System.EventHandler(this.console_Click);
+            // 
+            // settings
+            // 
+            this.settings.BackColor = System.Drawing.Color.White;
+            this.settings.BackgroundImage = global::VXPMenu.Properties.Resources.gear65;
+            this.settings.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.settings.FlatAppearance.BorderColor = System.Drawing.Color.LightGray;
+            this.settings.FlatAppearance.BorderSize = 0;
+            this.settings.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.settings.Location = new System.Drawing.Point(0, 321);
+            this.settings.Name = "settings";
+            this.settings.Size = new System.Drawing.Size(75, 133);
+            this.settings.TabIndex = 7;
+            this.settings.UseVisualStyleBackColor = false;
+            this.settings.Click += new System.EventHandler(this.settings_Click);
+            // 
+            // emulators
+            // 
+            this.emulators.BackColor = System.Drawing.Color.White;
+            this.emulators.BackgroundImage = global::VXPMenu.Properties.Resources.videogame;
+            this.emulators.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.emulators.FlatAppearance.BorderColor = System.Drawing.Color.LightGray;
+            this.emulators.FlatAppearance.BorderSize = 0;
+            this.emulators.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.emulators.Location = new System.Drawing.Point(0, 174);
+            this.emulators.Name = "emulators";
+            this.emulators.Size = new System.Drawing.Size(75, 128);
+            this.emulators.TabIndex = 6;
+            this.emulators.UseVisualStyleBackColor = false;
+            this.emulators.Click += new System.EventHandler(this.emulators_Click);
+            // 
+            // games
+            // 
+            this.games.BackColor = System.Drawing.Color.White;
+            this.games.BackgroundImage = global::VXPMenu.Properties.Resources.gamecontroller1;
+            this.games.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.games.FlatAppearance.BorderColor = System.Drawing.Color.LightGray;
+            this.games.FlatAppearance.BorderSize = 0;
+            this.games.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.games.Font = new System.Drawing.Font("Segoe UI Light", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.games.Location = new System.Drawing.Point(0, 12);
+            this.games.Name = "games";
+            this.games.Size = new System.Drawing.Size(75, 156);
+            this.games.TabIndex = 5;
+            this.games.UseVisualStyleBackColor = false;
+            this.games.Click += new System.EventHandler(this.games_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(480, 320);
+            this.Controls.Add(this.powerView);
+            this.Controls.Add(this.consoleView);
+            this.Controls.Add(this.settingView);
+            this.Controls.Add(this.emulatorView);
+            this.Controls.Add(this.gameView);
+            this.Controls.Add(this.power);
+            this.Controls.Add(this.console);
+            this.Controls.Add(this.settings);
+            this.Controls.Add(this.emulators);
+            this.Controls.Add(this.games);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button games;
+        private System.Windows.Forms.Button emulators;
+        private System.Windows.Forms.Button settings;
+        private System.Windows.Forms.Button console;
+        private System.Windows.Forms.Button power;
+        private System.Windows.Forms.Panel gameView;
+        private System.Windows.Forms.Panel emulatorView;
+        private System.Windows.Forms.Panel settingView;
+        private System.Windows.Forms.Panel consoleView;
+        private System.Windows.Forms.Panel powerView;
+    }
+}
+
---
+++ VXPMenu/Form1.cs
@@ -0,0 +1,331 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VXPMenu
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        struct PathMono
+        {
+            public string path;
+            public bool isMono;
+        }
+
+        List<Button> tabs = new List<Button>();
+        List<Panel> tabPanels = new List<Panel>();
+        //       game name,           path,  isMono
+        Dictionary<string, PathMono> gameList = new Dictionary<string, PathMono>();
+        Dictionary<string, PathMono> emulatorList = new Dictionary<string, PathMono>();
+        Dictionary<string, Bitmap> iconList = new Dictionary<string, Bitmap>();
+        Color mainColor = Color.White;
+        Color secondaryColor = Color.LightGray;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            int w = this.Width;
+            int h = this.Height;
+
+            tabPanels.Add(gameView);
+            tabPanels.Add(emulatorView);
+            tabPanels.Add(settingView);
+            tabPanels.Add(consoleView);
+            tabPanels.Add(powerView);
+
+            tabs.Add(games);
+            tabs.Add(emulators);
+            tabs.Add(settings);
+            tabs.Add(console);
+            tabs.Add(power);
+
+            iconList.Add("Quake 3", global::VXPMenu.Properties.Resources.quake3);
+            iconList.Add("Minecraft Pi", global::VXPMenu.Properties.Resources.minecraft);
+
+            StreamReader settingReader = new StreamReader("Resources/settings.txt");
+            string content;
+            while((content = settingReader.ReadLine()) != null) {
+                RegexOptions options = RegexOptions.None;
+                Regex regex = new Regex(@"((""((?<token>.*?)(?<!\\)"")|(?<token>[\w]+))(\s)*)", options);
+                var result = (from Match m in regex.Matches(content)
+                              where m.Groups["token"].Success
+                              select m.Groups["token"].Value).ToList();
+
+                switch(result[0])
+                {
+                    case "game":
+                        PathMono pm = new PathMono();
+                        pm.path = result[2];
+                        switch (result[3])
+                        {
+                            case "false":
+                                pm.isMono = false;
+                                break;
+
+                            case "true":
+                                pm.isMono = true;
+                                break;
+                        }
+                        gameList.Add(result[1], pm);
+                        break;
+
+                    case "emulator":
+                        PathMono _pm = new PathMono();
+                        pm.path = result[2];
+                        switch (result[3])
+                        {
+                            case "false":
+                                pm.isMono = false;
+                                break;
+
+                            case "true":
+                                pm.isMono = true;
+                                break;
+                        }
+                        emulatorList.Add(result[1], _pm);
+                        break;
+
+                    case "setting":
+                        switch(result[1])
+                        {
+                            case "primarycolor":
+                                mainColor = Color.FromArgb(Convert.ToInt16(result[2]), Convert.ToInt16(result[3]), Convert.ToInt16(result[4]));
+                                break;
+
+                            case "secondarycolor":
+                                secondaryColor = Color.FromArgb(Convert.ToInt16(result[2]), Convert.ToInt16(result[3]), Convert.ToInt16(result[4]));
+                                break;
+                        }
+                        break;
+                }
+            }
+
+            int gameID = 0;
+            int gameIDRow = 0;
+            int gamesPerRow = 0;
+
+            foreach (KeyValuePair<string, PathMono> gameKV in gameList)
+            {
+                string name = gameKV.Key;
+                string path = gameKV.Value.path;
+                bool isMono = gameKV.Value.isMono;
+
+                Button _l = new Button();
+                _l.FlatStyle = FlatStyle.Flat;
+                _l.BackColor = mainColor;
+                if (iconList.ContainsKey(name))
+                {
+                    _l.BackgroundImage = iconList[name];
+                }
+                else
+                {
+                    _l.BackgroundImage = global::VXPMenu.Properties.Resources.unknown;
+                }
+                _l.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+                _l.FlatAppearance.BorderColor = secondaryColor;
+                _l.FlatAppearance.BorderSize = 0;
+                _l.Font = new Font("Segoe UI Light", 12);
+                _l.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+                _l.Location = new System.Drawing.Point(0, 0);
+                _l.Text = name;
+                _l.Click += new EventHandler(delegate (object _sender, EventArgs _e) { OpenApplication(path, isMono); });
+                _l.TextAlign = ContentAlignment.BottomLeft;
+                _l.Size = new System.Drawing.Size((int)(w / 4.2f), (int)(w / 4.2f));
+                _l.Location = new Point((int)(10 + gameID * w / 4.2f + (gameID * 10)), 10 + ((gameIDRow) * _l.Height) + (gameIDRow * 10));
+                _l.TabIndex = 9;
+                _l.UseVisualStyleBackColor = false;
+
+                gameView.Controls.Add(_l);
+
+                gameID++;
+                if (gameView.Width - (_l.Location.X) < _l.Width / 2)
+                {
+                    gameIDRow++;
+                    gamesPerRow = gameID;
+                    gameID = 0;
+                }
+            }
+
+            int emulatorID = 0;
+            int emulatorIDRow = 0;
+            int emulatorsPerRow = 0;
+
+            foreach (KeyValuePair<string, PathMono> emulatorKV in emulatorList)
+            {
+                string name = emulatorKV.Key;
+                string path = emulatorKV.Value.path;
+                bool isMono = emulatorKV.Value.isMono;
+
+                Button _l = new Button();
+                _l.FlatStyle = FlatStyle.Flat;
+                _l.BackColor = mainColor;
+                if (iconList.ContainsKey(name))
+                {
+                    _l.BackgroundImage = iconList[name];
+                }
+                else
+                {
+                    _l.BackgroundImage = global::VXPMenu.Properties.Resources.unknown;
+                }
+                _l.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+                _l.FlatAppearance.BorderColor = secondaryColor;
+                _l.FlatAppearance.BorderSize = 0;
+                _l.Font = new Font("Segoe UI Light", 12);
+                _l.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+                _l.Location = new System.Drawing.Point(0, 0);
+                _l.Text = name;
+                _l.Click += new EventHandler(delegate (object _sender, EventArgs _e) { OpenApplication(path, isMono); });
+                _l.TextAlign = ContentAlignment.BottomLeft;
+                _l.Size = new System.Drawing.Size((int)(w / 4.2f), (int)(w / 4.2f));
+                _l.Location = new Point((int)(10 + emulatorID * w / 4.2f + (emulatorID * 10)), 10 + ((emulatorIDRow) * _l.Height) + (emulatorIDRow * 10));
+                _l.TabIndex = 9;
+                _l.UseVisualStyleBackColor = false;
+
+                emulatorView.Controls.Add(_l);
+
+                emulatorID++;
+                if (emulatorView.Width - (_l.Location.X) < _l.Width / 2)
+                {
+                    emulatorIDRow++;
+                    emulatorsPerRow = emulatorID;
+                    emulatorID = 0;
+                }
+            }
+
+            this.BackColor = secondaryColor;       
+               
+            games.Location = new Point(0, 0);
+            games.Size = new Size(w/6, h / tabs.Count);
+            games.BackColor = mainColor;
+
+            emulators.Location = new Point(0, h / tabs.Count);
+            emulators.Size = new Size(w / 6, h / tabs.Count);
+            emulators.BackColor = mainColor;
+
+            settings.Location = new Point(0, (h / tabs.Count) * 2);
+            settings.Size = new Size(w / 6, h / tabs.Count);
+            settings.BackColor = mainColor;
+
+            console.Location = new Point(0, (h / tabs.Count) * 3);
+            console.Size = new Size(w / 6, h / tabs.Count);
+            console.BackColor = mainColor;
+
+            power.Location = new Point(0, (h / tabs.Count) * 4);
+            power.Size = new Size(w / 6, h / tabs.Count);
+            power.BackColor = mainColor;
+
+            gameView.Location = new Point(w / 6, 0);
+            gameView.Size = new Size(w - w / 6, h);
+            gameView.Visible = false;
+            gameView.BackColor = secondaryColor;
+            gameView.AutoScroll = true;
+
+            emulatorView.Location = new Point(w / 6, 0);
+            emulatorView.Size = new Size(w - w / 6, h);
+            emulatorView.Visible = false;
+            emulatorView.BackColor = secondaryColor;
+            emulatorView.AutoScroll = true;
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Escape)
+            {
+                Application.Exit();
+            }
+        }
+
+        private void games_Click(object sender, EventArgs e)
+        {
+            foreach(Button _b in tabs)
+            {
+                _b.BackColor = mainColor;
+            }
+            games.BackColor = secondaryColor;
+
+            foreach (Panel _p in tabPanels)
+            {
+                _p.Visible = false;
+            }
+            gameView.Visible = true;
+        }
+
+        private void emulators_Click(object sender, EventArgs e)
+        {
+            foreach (Button _b in tabs)
+            {
+                _b.BackColor = mainColor;
+            }
+            emulators.BackColor = secondaryColor;
+
+            foreach (Panel _p in tabPanels)
+            {
+                _p.Visible = false;
+            }
+            emulatorView.Visible = true;
+        }
+
+        private void settings_Click(object sender, EventArgs e)
+        {
+            foreach (Button _b in tabs)
+            {
+                _b.BackColor = mainColor;
+            }
+            settings.BackColor = secondaryColor;
+
+            foreach (Panel _p in tabPanels)
+            {
+                _p.Visible = false;
+            }
+            settingView.Visible = true;
+        }
+
+        private void console_Click(object sender, EventArgs e)
+        {
+            foreach (Button _b in tabs)
+            {
+                _b.BackColor = mainColor;
+            }
+            console.BackColor = secondaryColor;
+
+            foreach (Panel _p in tabPanels)
+            {
+                _p.Visible = false;
+            }
+            consoleView.Visible = true;
+        }
+
+        private void power_Click(object sender, EventArgs e)
+        {
+            foreach (Button _b in tabs)
+            {
+                _b.BackColor = mainColor;
+            }
+            power.BackColor = secondaryColor;
+
+            foreach (Panel _p in tabPanels)
+            {
+                _p.Visible = false;
+            }
+            powerView.Visible = true;
+        }
+
+        public void OpenApplication(string path, bool isMono)
+        {
+            Debug.Print("Opening program @ " + path + "!");
+        }
+    }
+}
